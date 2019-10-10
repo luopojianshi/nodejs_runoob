937f75fb0a03d2155b9a67d8fb1d77a84bf150e3
lijiafu <lijiafu@backustech.com>
2019-10-10 15:17:13 +0800

    Node.js REPL(交互式解释器)

---
+++ course5/test.js
@@ -0,0 +1,25 @@
+1 +4
+5 / 2
+3 * 6
+4 -1
+1 + (2*3) - 4
+x = 10
+var y  = 10
+console.log(y)
+console.log(x)
+do {
+x++;
+console.log('x: ' + x);
+} while ( x < 5);
+var x = 0
+do {
+x++;
+console.log('x: ' + x);
+} while ( x < 5);
+_
+var x = 10
+var y = 20
+x + y
+_
+var sum = _
+console.log(sum)
